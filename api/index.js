b36fed58c1a49cfacce11eb8aedc97b832ab5118
JoaquinDW <balthasardeweert@gmail.com>
2023-01-13 16:54:42 -0300

    dotenv

--- api/index.js
+++ api/index.js
@@ -19,10 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require('dotenv').config()
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('is listening at 3001'); 
+  server.listen(process.env.PORT, () => {
+    console.log('is listening at', process.env.PORT); 
   });
 });
